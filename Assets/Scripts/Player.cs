5ecceb9e395650d62f7485ba557447438fe39bc1
Kim, Jaeseong <06zzkimzz06@gmail.com>
2022-11-24 00:03:11 +0900

    PlayerAnimation
    Stand, Run, Die 애니메이션 구현

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,13 +6,18 @@ using UnityEngine;
 public class Player : LivingEntity
 {
     // Player에서 사용할 변수 선언
-    public Rigidbody2D playerRigidbody;
+    Rigidbody2D playerRigidbody;
+    SpriteRenderer spriteRenderer;
+    Animator animator;
+
     public float speed = 5f;
     float h, v;
     
     void Start()
     {
         playerRigidbody = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
     }
 
     void Update()
@@ -22,13 +27,27 @@ public class Player : LivingEntity
 
     void Move()
     {
+        // 키입력 받음
         h = Input.GetAxisRaw("Horizontal");
         v = Input.GetAxisRaw("Vertical");
 
+        // Run 애니메이션 적용
+        anim(new Vector2 (h, v));
+
+        // 좌우반전여부
+        if(h != 0)
+        {
+            spriteRenderer.flipX = h < 0 ? true : false;
+        }
         // Move
         Vector3 curPos = transform.position;
         Vector3 nextPos = new Vector3(h, v, 0) * speed * Time.deltaTime;
         transform.position = curPos + nextPos;
+    }
+
+    void anim(Vector2 vector)
+    {
+        animator.SetFloat("Speed", vector.magnitude);
     }
 
     // 적과의 충돌처리
